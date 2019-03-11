eac226369ccd4a36c5170b68e82cd29e4908a23f
kennisburg <kennyhong1@live.com>
2019-03-10 17:00:44 -0700

    setup puppeteer, first page, headless:false

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const edgarSearch = 'https://www.sec.gov/edgar/searchedgar/companysearch.html';
 const ticker = process.argv[2];
 
 const puppeteer = require('puppeteer');
+
+(async() => {
+  const browser = await puppeteer.launch({headless:false});
+  const page = await browser.newPage();
+
+  await page.goto(edgarSearch)
+
+
+
+  await browser.close();
+})();
