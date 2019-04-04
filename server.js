56ac52f068f27ce549adbcb59fc5ea3c08820866
kennisburg <kennyhong1@live.com>
2019-04-04 12:25:47 -0700

    server start, parse cheerio json

--- server.js
+++ server.js
@@ -4,3 +4,20 @@ const port = 8080;
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) => {
+  res.send(`<html>
+              <body>
+                Get 10Qs
+              </body>
+            </html>\n`);
+});
+
+
+app.listen(port, () => {
+  console.log(`go to localhost:${port}`)
+});
--- test.js
+++ test.js
@@ -1,1 +1,17 @@
-```npm i sequelize pg pg-hstore```
+const test = 'https://www.sec.gov/Archives/edgar/data/24741/000002474118000053/glw-20180930x10q.htm'
+
+const request = require('request');
+
+const cheerio = require('cheerio');
+
+request(test, (error, res, html) => {
+  if (!error && res.statusCode == 200) {
+    // console.log(html)
+
+    const $ = cheerio.load(html)
+
+    const table = $('tbody')
+
+    console.log(table)
+  }
+})
